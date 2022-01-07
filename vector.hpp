7fa83fa27e90f5e75a36f08890874530cc65d93e
akh9001 <akhalidy9001@gmail.com>
2022-01-07 20:49:29 +0100

    scratch

---
+++ vector.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+
+//* Default Parameters
+
+namespace ft
+{
+template < class T, class Alloc = std::allocator<T>>
+class vector
+{
+	private:
+
+	public:
+	typedef T value_type;
+
+
+};
+
+	
+
+};
